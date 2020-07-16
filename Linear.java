321a8f8b9124b2a37fe04d6a4a4adc26b170412c
tanishsaajan <54498364+tanishsaajan@users.noreply.github.com>
2020-07-16 21:33:09 +0530

    Update Linear.java

--- Linear.java
+++ Linear.java
@@ -1,25 +1,38 @@
-class GFG  
-{  
-public static int search(int arr[], int x) 
-{ 
+import java.util.*;
+class Main{
+ public static void main(String args[]){  
+
+Scanner s=new Scanner(System.in);
+int size=s.nextInt();
+        int arr[] = new int[size]; 
+for(int i=0;i<arr.length;i++)
+{
+    arr[i]=s.nextInt();
+}
+int x=s.nextInt();
+int y=0;
+int g=0;
     int n = arr.length; 
     for(int i = 0; i < n; i++) 
     { 
-        if(arr[i] == x) 
-            return i; 
+        if(arr[i] == x)
+        {
+           y=1;
+           g=i;
+        }
+        
+        
+            
     } 
-    return -1; 
-} 
-  
-public static void main(String args[]) 
-{ 
-    int arr[] = { 2, 3, 4, 10, 40 };  
-    int x = 10; 
-      
-    int result = search(arr, x); 
-    if(result == -1) 
-        System.out.print("Element is not present in array"); 
+   
+
+    if(y == 1) 
+    
+        System.out.print("Element is present at index " + g); 
     else
-        System.out.print("Element is present at index " + result); 
-} 
-} 
+
+        System.out.print("Element is not present in array"); 
+
+ }  
+
+}  
