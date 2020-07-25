9b22f00d05f6a67ca9479948d48a705e8cc985c8
tanishsaajan <54498364+tanishsaajan@users.noreply.github.com>
2020-07-25 19:51:33 +0530

    Add files via upload

---
+++ InsertAtBeg.java
@@ -0,0 +1,54 @@
+public class Main
+{
+
+	Node head;
+	static class Node
+	{
+		int data;
+		Node next;
+		Node(int x)
+		{
+			data=x;
+			next=null;
+		}
+	}
+  public static Main insertAtBeg(Main list , int data)
+	{
+		Node data1=new Node(data);
+		if(list.head==null)
+		{
+			list.head=data1;
+		}
+		else
+		{
+			Node temp=list.head;
+			data1.next=temp;
+			temp=data1;
+		}
+		return list;
+	}
+	static void printlist(Main list)
+	{
+		Node n2=list.head;
+		while(n2.next!=null)
+		{
+			System.out.println(n2.data);
+			n2=n2.next;
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Main list=new Main();
+		list=insertAtBeg(list,1);
+		list=insertAtBeg(list,2);
+		list=insertAtBeg(list,3);
+		list=insertAtBeg(list,4);
+		list=insertAtBeg(list,5);
+		list=insertAtBeg(list,6);
+		list=insertAtBeg(list,7);
+		list=insertAtBeg(list,8);
+		list=insertAtBeg(list,9);
+		list=insertAtBeg(list,10);
+		printlist(list);
+	}
+}
